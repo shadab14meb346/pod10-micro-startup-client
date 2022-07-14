48ade4397fc2368acb1bd287954b06ea981ecb33
shadab <shadab14meb346@gmail.com>
2022-07-15 00:39:13 +0530

    fix environment variable issue

--- pages/api/get-user-category.ts
+++ pages/api/get-user-category.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 import {
   convertMultilineTweetsToOneLine,
@@ -11,59 +12,70 @@ export default async function handler(
 ) {
   if (req.method === "GET") {
     const username = req.query["username"];
-    const axios = require("axios");
 
-    const fetchTweetsOfUser = async (username: string, wordToMatch: string) => {
-      try {
-        const response = await axios.get(
-          `https://api.twitter.com/2/tweets/search/recent?query=-is%3Areply%20-is%3Aretweet%20${wordToMatch}%20(from%3A%20${username})&tweet.fields=&max_results=100`,
-          {
-            headers: {
-              Authorization:
-                "Bearer AAAAAAAAAAAAAAAAAAAAAPhjWAEAAAAA0s%2FWHeOO%2BvP96lKjfw6uZBgWCzI%3DAj6em26R9yL15TuETqDuXYVphaLRGFnTPgWKZtm3puPWjfTx04",
-              Cookie:
-                'guest_id=v1%3A165754080682838762; guest_id_ads=v1%3A165754080682838762; guest_id_marketing=v1%3A165754080682838762; personalization_id="v1_Sy1q3AX5HK750dL3bNbuVg=="',
-            },
-          }
-        );
-        if (response.data.meta.result_count > 0) {
-          return response.data.data.map((item: any) =>
-            convertMultilineTweetsToOneLine(item.text)
+    try {
+      const fetchTweetsOfUser = async (
+        username: string,
+        wordToMatch: string
+      ) => {
+        try {
+          const response = await axios.get(
+            `https://api.twitter.com/2/tweets/search/recent?query=-is%3Areply%20-is%3Aretweet%20${wordToMatch}%20(from%3A%20${username})&tweet.fields=&max_results=100`,
+            {
+              headers: {
+                Authorization:
+                  "Bearer AAAAAAAAAAAAAAAAAAAAAPhjWAEAAAAA0s%2FWHeOO%2BvP96lKjfw6uZBgWCzI%3DAj6em26R9yL15TuETqDuXYVphaLRGFnTPgWKZtm3puPWjfTx04",
+                Cookie:
+                  'guest_id=v1%3A165754080682838762; guest_id_ads=v1%3A165754080682838762; guest_id_marketing=v1%3A165754080682838762; personalization_id="v1_Sy1q3AX5HK750dL3bNbuVg=="',
+              },
+            }
           );
+          if (response.data.meta.result_count > 0) {
+            return response.data.data.map((item: any) =>
+              convertMultilineTweetsToOneLine(item.text)
+            );
+          }
+          return [];
+        } catch (e) {
+          console.log(e);
         }
-        return [];
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    const fetchAllRecentTweetsOfUserWithTargetKeywords = async (
-      username: string
-    ) => {
-      const requests = keywordsToMatch.map((keyword) =>
-        fetchTweetsOfUser(username, keyword)
-      );
-      const results = await Promise.all(requests);
-      const nonEmptyResults = results.filter((item) => item.length);
-      return nonEmptyResults.flat();
-    };
-    const getUserCategory = async (username: string) => {
-      const relevantTweetsText =
-        await fetchAllRecentTweetsOfUserWithTargetKeywords(username);
-      if (relevantTweetsText.length) {
-        //@ts-ignore
-        const category = await fetchTextCategory(relevantTweetsText);
-        return category;
-      } else {
-        console.log("no relevant Tweets found");
-        return "NA";
-      }
-    };
-    const category = await getUserCategory(username as string);
-
-    res.status(200).json({
-      data: {
-        category: category,
-      },
-    });
+      };
+      const fetchAllRecentTweetsOfUserWithTargetKeywords = async (
+        username: string
+      ) => {
+        const requests = keywordsToMatch.map((keyword) =>
+          fetchTweetsOfUser(username, keyword)
+        );
+        const results = await Promise.all(requests);
+        const nonEmptyResults = results.filter((item) => item.length);
+        return nonEmptyResults.flat();
+      };
+      const getUserCategory = async (username: string) => {
+        const relevantTweetsText =
+          await fetchAllRecentTweetsOfUserWithTargetKeywords(username);
+        const textInOneLine = relevantTweetsText.join(",");
+        if (textInOneLine) {
+          //@ts-ignore
+          const category = await fetchTextCategory(textInOneLine);
+          return category;
+        } else {
+          console.log("no relevant Tweets found");
+          return "NA";
+        }
+      };
+      const category = await getUserCategory(username as string);
+      res.status(200).json({
+        data: {
+          category: category,
+        },
+      });
+    } catch (e) {
+      // console.log(e);
+      res.status(500).json({
+        data: {
+          error: "something went wrong",
+        },
+      });
+    }
   }
 }
--- utils/index.ts
+++ utils/index.ts
@@ -1,26 +1,30 @@
 import { Configuration, OpenAIApi } from "openai";
 import { defaultPrompt } from "./const";
 
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const configuration = new Configuration({
   apiKey: OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 export const fetchTextCategory = async (text: string) => {
-  const prompt = `${defaultPrompt}\ntext: ${text}\ncategory:`;
-  const response = await openai.createCompletion({
-    model: "text-davinci-002",
-    prompt,
-    temperature: 0.7,
-    max_tokens: 256,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  });
-  //@ts-ignore
-  const category = response?.data?.choices[0]?.text?.trim();
-  return category;
+  try {
+    const prompt = `${defaultPrompt}\ntext: ${text}\ncategory:`;
+    const response = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt,
+      temperature: 0.7,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+    //@ts-ignore
+    const category = response?.data?.choices[0]?.text?.trim();
+    return category;
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 export const convertMultilineTweetsToOneLine = (text: string) => {
